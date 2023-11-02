a21c1495eb337962fab3c14a69838c8917dc372d
Kelson <kelsonbatista@gmail.com>
2023-11-02 18:08:38 -0300

    added migration column not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_170517) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_210510) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_02_170517) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
